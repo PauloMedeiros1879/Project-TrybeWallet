82fd73d2ee4be11aa8311ae3fdfef914a0f18cd1
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-05-20 16:37:50 -0400

    Descarta alterações

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,42 +1,7 @@
-import { getExpenses, getCurrencies } from '../services/index';
 // Coloque aqui suas actions
 export const LOGIN = 'LOGIN';
-export const ADD_EXPENSES = 'ADD_DATA_EXPENSES';
-export const DELETE_EXPENSES = 'DELETE_EXPENSES';
-export const REQUEST_API_SUCCESS = 'REQUEST_API_SUCESS';
 
 export const userLogin = (payload) => ({
   type: LOGIN,
   payload,
-});
-
-const requestSuccess = (currencies) => ({
-  type: REQUEST_API_SUCCESS,
-  payload: { currencies },
-});
-
-export const getCurrenciesApi = () => async (dispatch) => {
-  const allCurrencies = await getCurrencies();
-  const FilteredCurrencies = allCurrencies
-    .filter((currency) => (currency !== 'USDT' && currency));
-  dispatch(requestSuccess(FilteredCurrencies));
-};
-
-export const addExpenses = (expenses) => ({
-  type: ADD_EXPENSES,
-  payload: { expenses },
-});
-
-export const getExpensesApi = (state) => async (dispatch) => {
-  const exchangeRates = await getExpenses();
-  const newState = {
-    ...state,
-    exchangeRates,
-  };
-  dispatch(addExpenses(newState));
-};
-
-export const deleteExpenses = (id) => ({
-  type: DELETE_EXPENSES,
-  payload: { id },
 });
