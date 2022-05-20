e3c105e7b2fb2be09dc4908a2e4d94b24270f0ca
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-05-20 15:48:28 -0400

    Tentando algumas configurações para o requisito 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,42 @@
+import { getExpenses, getCurrencies } from '../services/index';
 // Coloque aqui suas actions
 export const LOGIN = 'LOGIN';
-export const USER = 'USER';
+export const ADD_EXPENSES = 'ADD_DATA_EXPENSES';
+export const DELETE_EXPENSES = 'DELETE_EXPENSES';
+export const REQUEST_API_SUCCESS = 'REQUEST_API_SUCESS';
 
 export const userLogin = (payload) => ({
   type: LOGIN,
   payload,
 });
 
-export const userAction = (payload) => ({
-  type: USER,
-  payload,
+const requestSuccess = (currencies) => ({
+  type: REQUEST_API_SUCCESS,
+  payload: { currencies },
+});
+
+export const getCurrenciesApi = () => async (dispatch) => {
+  const allCurrencies = await getCurrencies();
+  const FilteredCurrencies = allCurrencies
+    .filter((currency) => (currency !== 'USDT' && currency));
+  dispatch(requestSuccess(FilteredCurrencies));
+};
+
+export const addExpenses = (expenses) => ({
+  type: ADD_EXPENSES,
+  payload: { expenses },
+});
+
+export const getExpensesApi = (state) => async (dispatch) => {
+  const exchangeRates = await getExpenses();
+  const newState = {
+    ...state,
+    exchangeRates,
+  };
+  dispatch(addExpenses(newState));
+};
+
+export const deleteExpenses = (id) => ({
+  type: DELETE_EXPENSES,
+  payload: { id },
 });
