456476aa8fe733d3679a3a15729ed9a6314401b3
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-05-24 06:17:38 -0400

    Conserta o reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
-import { user } from '../actions/index';
+import { LOGIN } from '../actions/index';
 
 const INITAL_STATE = ({
   email: 'alguem@email.com',
@@ -7,7 +7,7 @@ const INITAL_STATE = ({
 
 export default function reduceUser(state = INITAL_STATE, action) {
   switch (action.type) {
-  case user:
+  case LOGIN:
     return {
       ...state,
       email: action.payload,
